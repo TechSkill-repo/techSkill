8de4f47863f3b6bfd508c6c2f68db78aafdcb35a
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-04-12 02:37:08 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Cards from "./components/Cards";
 import HomeContainer from "./pages/HomeContainer";
 import Frontend from "./pages/Frontend";
+import MobileAppDev from "./pages/MobileAppDev";
+import Contact from "./pages/Contact";
+import FullStack from "./pages/FullStack";
+import Python from "./pages/Python";
 
 function App() {
   return (
     <div className="w-screen h-auto flex flex-col bg-primary">
-        <Navbar />
-        <main className=' w-full'>
-          <Routes>
-            <Route path="/*" element={<HomeContainer />} />
-            <Route path="/frontend-dev" element={<Frontend />} />
-          </Routes>
-        </main>
-      </div>
+      <Navbar />
+      <main className=" w-full">
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<HomeContainer />} />
+          <Route path="/frontend-dev" element={<Frontend />} />
+          <Route path="/mobileapp-dev" element={<MobileAppDev />} />
+          <Route path="/fullstack" element={<FullStack />} />
+          <Route path="/python" element={<Python />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
